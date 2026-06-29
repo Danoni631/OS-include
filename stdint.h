9b9ff77f46f0a850b573a83f5eadab450c036967
Danoni631 <danunisonsoi@gmail.com>
2026-06-29 19:07:39 -0300

    Add files via upload

---
+++ stdint.h
@@ -0,0 +1,90 @@
+typedef unsigned char  uint8_t,  BYTE, UCHAR;
+typedef unsigned short uint16_t, WORD, UWORD;
+typedef unsigned int   uint32_t, DWORD, UINT;
+typedef unsigned long  ULONG;
+typedef unsigned short USHORT;
+typedef unsigned int size_t;
+typedef unsigned long long uint64_t, QWORD, ULLONG;
+
+typedef char   CHAR;
+typedef short  SHORT;
+typedef int    INT;
+typedef void   VOID;
+typedef float  FLOAT;
+typedef double DOUBLE;
+typedef long   LONG;
+
+typedef BYTE*  LPBYTE;
+typedef WORD*  LPWORD;
+typedef DWORD* LPDWORD;
+typedef VOID* LPVOID;
+typedef CHAR* LPCHAR;
+typedef UWORD* LPUWORD;
+typedef USHORT* LPUSHORT;
+typedef UCHAR* LPUCHAR;
+
+typedef const char* STRING;
+
+#define NULL  ((void*)0)
+
+#define TRUE  1
+#define true  1
+
+#define FALSE 0
+#define false 0
+
+#define INT8_MIN         (-127i8 - 1)
+#define INT16_MIN        (-32767i16 - 1)
+#define INT32_MIN        (-2147483647i32 - 1)
+#define INT64_MIN        (-9223372036854775807i64 - 1)
+#define INT8_MAX         127i8
+#define INT16_MAX        32767i16
+#define INT32_MAX        2147483647i32
+#define INT64_MAX        9223372036854775807i64
+#define UINT8_MAX        0xffui8
+#define UINT16_MAX       0xffffui16
+#define UINT32_MAX       0xffffffffui32
+#define UINT64_MAX       0xffffffffffffffffui64
+
+#define INT_LEAST8_MIN   INT8_MIN
+#define INT_LEAST16_MIN  INT16_MIN
+#define INT_LEAST32_MIN  INT32_MIN
+#define INT_LEAST64_MIN  INT64_MIN
+#define INT_LEAST8_MAX   INT8_MAX
+#define INT_LEAST16_MAX  INT16_MAX
+#define INT_LEAST32_MAX  INT32_MAX
+#define INT_LEAST64_MAX  INT64_MAX
+#define UINT_LEAST8_MAX  UINT8_MAX
+#define UINT_LEAST16_MAX UINT16_MAX
+#define UINT_LEAST32_MAX UINT32_MAX
+#define UINT_LEAST64_MAX UINT64_MAX
+
+#define INT_FAST8_MIN    INT8_MIN
+#define INT_FAST16_MIN   INT32_MIN
+#define INT_FAST32_MIN   INT32_MIN
+#define INT_FAST64_MIN   INT64_MIN
+#define INT_FAST8_MAX    INT8_MAX
+#define INT_FAST16_MAX   INT32_MAX
+#define INT_FAST32_MAX   INT32_MAX
+#define INT_FAST64_MAX   INT64_MAX
+#define UINT_FAST8_MAX   UINT8_MAX
+#define UINT_FAST16_MAX  UINT32_MAX
+#define UINT_FAST32_MAX  UINT32_MAX
+#define UINT_FAST64_MAX  UINT64_MAX
+
+#ifdef _WIN64
+    #define INTPTR_MIN   INT64_MIN
+    #define INTPTR_MAX   INT64_MAX
+    #define UINTPTR_MAX  UINT64_MAX
+#else
+    #define INTPTR_MIN   INT32_MIN
+    #define INTPTR_MAX   INT32_MAX
+    #define UINTPTR_MAX  UINT32_MAX
+#endif
+
+#define INTMAX_MIN       INT64_MIN
+#define INTMAX_MAX       INT64_MAX
+#define UINTMAX_MAX      UINT64_MAX
+
+#define PTRDIFF_MIN      INTPTR_MIN
+#define PTRDIFF_MAX      INTPTR_MAX
